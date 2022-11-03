77bb558b963766a8e05b2ff22f8eee109cef773b
danijel husakovic <danijel.husakovic@gmail.com>
2022-11-03 11:06:38 +0100

    Completed form Ucenici.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Knjiznica
+﻿namespace Knjiznica
 {
     partial class Form1
     {
@@ -29,13 +28,19 @@ namespace Knjiznica
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
---
+++ Model/PodatkovniKontekst.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Knjiznica.Model
+{
+    public class PodatkovniKontekst
+    {
+        // Kolekcije
+        public List<Ucenik> Ucenici;
+        public List<Knjiga> Knjige;
+        public List<Posudba> Posudbe;
+    }
+}
