0fbbef222211d8fb0e99555b10ef07afdae0e2a3
danijel husakovic <danijel.husakovic@gmail.com>
2022-11-03 12:38:37 +0100

    Completed loading students from a file.

--- Model/PodatkovniKontekst.cs
+++ Model/PodatkovniKontekst.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace Knjiznica.Model
 {
@@ -11,5 +12,42 @@ namespace Knjiznica.Model
         public List<Ucenik> Ucenici;
         public List<Knjiga> Knjige;
         public List<Posudba> Posudbe;
+
+        private string datUcenici = "ucenici.txt";
+
+        public PodatkovniKontekst()
+        {
+            Ucenici = UcitajUcenike();
+        }
+
+        public List<Ucenik> UcitajUcenike()
+        {
+            List<Ucenik> rezultat = new List<Ucenik>();
+
+            if(File.Exists(datUcenici))
+            {
+                using (StreamReader sr = new StreamReader(datUcenici))
+                {
+                    while(!sr.EndOfStream)
+                    {
+                        string linija = sr.ReadLine();
+                        // Splitamo liniju i definiramo objekt ucenik
+                        Ucenik trenutniUcenik = new Ucenik();
+                        string[] polja = linija.Split('|');
+                        trenutniUcenik.OIB = polja[0];
+                        trenutniUcenik.Ime = polja[1];
+                        trenutniUcenik.Prezime = polja[2];
+                        trenutniUcenik.Adresa = polja[3];
+                        trenutniUcenik.Telefon = polja[4];
+                        trenutniUcenik.Razred = int.Parse(polja[5]);
+
+                        // Dodajemo pročitanog učenik u listu
+                        rezultat.Add(trenutniUcenik);
+                    }
+                }
+            }
+
+            return rezultat;
+        }
     }
 }
