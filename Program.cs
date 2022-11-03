dec32c82b2a999a435427ae72207b55bf60a90dc
danijel husakovic <danijel.husakovic@gmail.com>
2022-11-03 11:46:55 +0100

    Completed form Knjiznica, and set it to be the starting form when running the application.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using Knjiznica.Forme;
 
 namespace Knjiznica
 {
@@ -15,7 +16,7 @@ namespace Knjiznica
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmKnjiznica());
         }
     }
 }
